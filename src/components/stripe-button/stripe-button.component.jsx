1436d433aebad25e5e3020bdea45fa32e709497b
npetronikolos <petronikolosn@gmail.com>
2019-09-30 09:38:09 +0200

    All components have been converted to styled-components

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -1,30 +1,29 @@
 import React from 'react';
-
 import StripeCheckout from 'react-stripe-checkout';
 
 const StripeCheckoutButton = ({ price }) => {
-    const priceForStripe = price * 100;
-    const publishableKey = 'pk_test_4GeIahqDdfS5zJbjv8KHAGXx00crsF0eB3'
+  const priceForStripe = price * 100;
+  const publishableKey = 'pk_test_WBqax2FWVzS9QlpJScO07iuL';
 
-const onToken = token => {
-        console.log(token);
-        alert('Payment Succesful');
-    }   
+  const onToken = token => {
+    console.log(token);
+    alert('Payment Succesful!');
+  };
 
-    return (
-        <StripeCheckout 
-            label='Pay Now'
-            name='CRWN Clothing LTD.'
-            billingAddress
-            shippingAddress
-            image='https://sendeyo.com/up/d/f3eb2117da'
-            description={`Your total is $${price}`}
-            amount={priceForStripe}
-            panelLabel='Pay Now'
-            token={onToken}
-            stripeKey={publishableKey}
-        />
-    );
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='CRWN Clothing Ltd.'
+      billingAddress
+      shippingAddress
+      image='https://svgshare.com/i/CUz.svg'
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
 };
 
 export default StripeCheckoutButton;
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -3,14 +3,13 @@ import React from 'react';
 import Directory from '../../components/directory/directory.component';
 
 
-import './homepage.styles.scss';
-
+import { HomePageContainer } from './homepage.styles';
 
 
 const HomePage =() => (
-    <div className= 'homegage'>
-       <Directory />
-    </div>
+   <HomePageContainer>
+        <Directory />
+   </HomePageContainer>
 );
 
 export default HomePage;
